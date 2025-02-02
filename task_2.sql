0ab0198873cc077ddbcac264769f09851511ff4a
samueljuma <samueljuma1024@gmail.com>
2025-02-02 10:06:05 +0300

    modify task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,44 +1,44 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Create the 'authors' table
-CREATE TABLE IF NOT EXISTS authors (
+-- Create the 'Authors' table
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create the 'books' table
-CREATE TABLE IF NOT EXISTS books (
+-- Create the 'Books' table
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE SET NULL
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Create the 'customers' table
-CREATE TABLE IF NOT EXISTS customers (
+-- Create the 'Customers' table
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL
+    address TEXT
 );
 
--- Create the 'orders' table
-CREATE TABLE IF NOT EXISTS orders (
+-- Create the 'Orders' table
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create the 'order_details' table
-CREATE TABLE IF NOT EXISTS order_details (
+-- Create the 'Order_Details' table
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL CHECK (quantity > 0),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
